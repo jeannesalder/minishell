4104c9458c3c02956a5b47f7a84a3d97f8d6d24f
hayaks <julien.sag36@gmail.com>
2021-01-06 16:18:10 +0100

    On continue sur les variables d'env

--- srcs/value_env.c
+++ srcs/value_env.c
@@ -14,20 +14,32 @@ int	search_env(int i, char **str, t_var *shell)
 		brace = 1;
 	if ((*str)[i + 1 + brace] == '?')
 		env = ft_itoa(shell->ret);
-	len = len_env(*str + i + 1 + brace, brace);
+	len = len_env(*str + i + 1 + brace);
+	if (!(env))
+	{
+		env = ft_strduplen(*str + i + 1 + braces, len - 1 - braces * 2);
+		//recuperer la valeur de la variable d'env et le mettre dans env
+	}
 	temp1 = ft_strduplen(*str, i);
+	temp2 = ft_strdup(*str + i + len);
+	len = ft_strlen(env);
+	aux = ft_strjoin(bef, env);
+	free(*str);
+	*str = ft_strjoin(ft_strjoin(temp1, env), temp2);
+	free(temp1);
+	free(temp2);
+	free(env);
+	return (len);
 }
 
-int		len_env(char *str, int brace)
+int		len_env(char *str)
 {
 	int		i;
 
 	i = 0;
 	while (str[i] && !(ft_isdigit(str[0])) 
-	&& ft_isalnum(str[i]) || str == '_'))
-	{
+	&& (ft_isalnum(str[i]) || str[i] == '_'))
 		i++;
-	}
 	return (i);
 }
 
