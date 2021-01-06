7accfcd772ba50b57abf37416b86c82ea5d39bed
hayaks <julien.sag36@gmail.com>
2021-01-06 15:24:14 +0100

    debut env

--- srcs/value_env.c
+++ srcs/value_env.c
@@ -1,7 +1,34 @@
 #include "./../includes/minishell.h"
 
-int	search_env(int i, char **str, t_mini *mini)
+int	search_env(int i, char **str, t_var *shell)
 {
+	char	*temp1;
+	char	*temp2;
+	char	*env;
+	int		len;
+	int		brace;
+
+	brace = 0;
+	env = 0;
+	if ((*str)[i + 1] == '{')
+		brace = 1;
+	if ((*str)[i + 1 + brace] == '?')
+		env = ft_itoa(shell->ret);
+	len = len_env(*str + i + 1 + brace, brace);
+	temp1 = ft_strduplen(*str, i);
+}
+
+int		len_env(char *str, int brace)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] && !(ft_isdigit(str[0])) 
+	&& ft_isalnum(str[i]) || str == '_'))
+	{
+		i++;
+	}
+	return (i);
 }
 
 void	value_env(t_var *shell, char **str)
